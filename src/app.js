90e237000f56f8353d06c41168b1dbd5a13af36c
joelbns7 <joelsbonus@gmail.com>
2023-09-14 15:25:54 -0300

    Modifique app.js, package y package-lock

--- src/app.js
+++ src/app.js
@@ -1,13 +1,36 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 3010;
 
 app.use(express.static(path.resolve(__dirname, 'public')));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'views/index.html'));
 });
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/views/register.html');
+});
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html');
+});
+app.get('/product', (req, res) => {
+    res.sendFile(__dirname + '/views/product.html');
+});
+app.get('/cart', (req, res) => {
+    res.sendFile(__dirname + '/views/cart.html');
+});
+app.post('/register', (req, res) => {
+    res.redirect('/');
+});
+app.post('/login', (req, res) => {
+    res.redirect('/');
+});
+app.get('*', (req, res) => {
+    res.status(404).send('Página no encontrada');
+});
+
 
 app.listen(port, () => {
     console.log(`Escuchando en el puerto ${port}`);
