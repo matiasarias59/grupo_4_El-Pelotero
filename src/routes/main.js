8689959706c1b9d6c983463fd69a309a22a9f400
Matias Arias <matiasarias59@gmail.com>
2023-12-02 20:31:55 -0300

    Add AuthMiddleware in cart route

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mainController= require('../controllers/mainController');
 
+const authMiddleware = require('../middlewares/authMiddleware');
+
 const router = express.Router();
 
 router.get('/', mainController.index);
 //router.get('/login', mainController.login);
 //router.get('/register', mainController.register);
-router.get('/cart', mainController.cart);
+router.get('/cart', authMiddleware, mainController.cart);
 //router.post('/login', mainController.loginUser);
 //router.post('/register', mainController.createUser);
 
