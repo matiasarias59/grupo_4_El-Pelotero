dabd28600bb43ab515ad51b7933ed83b22cf859c
benjameyer <benjameyer2022@gmail.com>
2024-01-10 16:54:31 -0300

    fix models assoc y add product CRUD

--- src/database/models/Brand.js
+++ src/database/models/Brand.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Brand.hasMany(models.Products,{
+      Brand.hasMany(models.Product,{
         foreignKey: 'brands_id',
         as:'products',
       })
